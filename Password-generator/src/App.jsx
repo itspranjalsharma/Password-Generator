4c5dbe33734d80f25fb98001374a9a83b90c1275
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-05-27 23:38:12 +0530

    68%

--- Password-generator/src/App.jsx
+++ Password-generator/src/App.jsx
@@ -25,16 +25,39 @@ function App() {
  
   return (
     <>
-    <div className=' w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'> 
-    <h1 className='text-white text-center my-3'> Password Generator </h1>
-    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-      <input type="text"
+    <div className=' w-full h-40 max-w-md mx-auto shadow-md  rounded-lg px-4 my-15 text-orange-500 bg-gray-700'> 
+      <h1 className='text-white text-center my-7 mx-3'> Password Generator </h1>
+     <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+       <input type="text"
         value={password}
-        className='outline-none w-full py-1 px3'
+        className='outline-none w-full py-1 px=3'
         placeholder='Password'
         readOnly
-      />
-    </div>
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py0.5 shrink-0' > copy</button>
+      </div>
+      <div className="flex text-sm gap-x-2"></div>
+      <div className="flex items-center gap-x-1">
+        <input 
+        type="rang" 
+        min={6}
+        max={80}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e) => {setLength(e.target.value)}}
+        />
+        <label > Length: {length}</label>
+      </div>
+        <input type="checkbox"
+          defaultChecked={numberAllow}
+          id='numberInput'
+          onChange={() =>{
+            setnumberAllow((prev) =>!prev);
+          }}
+          />
+
+          <label htmlFor="numberInput">Numbers</label>
+
     </div>
     </>
 
