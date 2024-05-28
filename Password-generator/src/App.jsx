dc9390342eae78b526226e99b3437eb490d508a6
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-05-28 22:34:01 +0530

    Completed

--- Password-generator/src/App.jsx
+++ Password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback } from 'react'
+import { useState , useCallback , useEffect ,useRef} from 'react'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,6 +7,9 @@ function App() {
   const [charAllow , setCharAllow] = useState(false)
   const [password , setPassword] = useState("")
 
+  // useRef hook use
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback( ()=>{
     let pass = ""
     let str =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -14,15 +17,23 @@ function App() {
     if(numberAllow) str += "0123456789"
     if(charAllow) str += "!@#$%^&*()[]{}"
 
-    for(let i = 1 ; i<Array.length ; i++) {
+    for(let i = 1 ; i<=length ; i++) {
       let char = Math.floor(Math.random() * str.length + 1 )
-      pass = str.charAt(char)
+      pass += str.charAt(char)
 
     }
 
     setPassword(pass)
   } , [length, numberAllow,charAllow,setPassword])
  
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,15)
+    window.navigator.clipboard.writeText(password)
+  } , [password])
+  useEffect(()=>{
+    passwordGenerator()
+  } , [length ,numberAllow ,charAllow , passwordGenerator])
   return (
     <>
     <div className=' w-4/5 h-40 max-w-md mx-auto shadow-md  rounded-lg px-4 my-15 text-orange-500 bg-gray-700'> 
@@ -33,15 +44,16 @@ function App() {
         className='outline-none w-full py-1 px=3'
         placeholder='Password'
         readOnly
+        ref={passwordRef}
         />
-        <button className='outline-none bg-blue-700 text-white px-3 py0.5 shrink-0' > copy</button>
+        <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py0.5 shrink-0' > copy</button>
       </div>
       <div className="flex text-sm gap-x-2">
       <div className="flex items-center gap-x-1">
         <input 
-        type="rang" 
+        type="range" 
         min={6}
-        max={80}
+        max={100}
         value={length}
         className='cursor-pointer'
         onChange={(e) => {setLength(e.target.value)}}
